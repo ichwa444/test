e2d95166baacc0280cc4da6626f593c45b8bbe92
mamarcha <mehdiamarcha123@gmail.com>
2024-08-15 11:27:45 +0100

    rr

--- Teeestt/main.go
+++ Teeestt/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.CheckNumber("Hello"))
-	fmt.Println(piscine.CheckNumber("Hello1"))
+	fmt.Println(piscine.RectPerimeter(10, 2))
+	fmt.Println(piscine.RectPerimeter(434343, 898989))
+	fmt.Println(piscine.RectPerimeter(10, -2))
 }
